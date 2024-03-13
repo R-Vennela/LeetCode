3276d7d511901c134a901986f15f208b4ccaaab9
R-Vennela <vennelaramavath@gmail.com>
2024-03-13 19:54:17 +0530

    adding Leetcode solutions

--- 1171. Remove Zero Sum Consecutive Nodes from Linked List.py
+++ 1171. Remove Zero Sum Consecutive Nodes from Linked List.py
@@ -1,22 +1,17 @@
 class Solution:
     def removeZeroSumSublists(self, head):
-        dummy = ListNode(0)
-        dummy.next = head
-        prefixSumToNode = {}
-        prefixSum = 0
-        current = dummy
-        while current:
-            prefixSum += current.val
-            if prefixSum in prefixSumToNode:
-                prev = prefixSumToNode[prefixSum]
-                toRemove = prev.next
-                p = prefixSum + (toRemove.val if toRemove else 0)
-                while p != prefixSum:
-                    del prefixSumToNode[p]
-                    toRemove = toRemove.next
-                    p += toRemove.val if toRemove else 0
-                prev.next = current.next
-            else:
-                prefixSumToNode[prefixSum] = current
-            current = current.next
-        return dummy.next      
+        dummy=ListNode()
+        dummy.next=head
+        ans=0
+        dict_={ans:dummy}
+        while head:
+            ans+=head.val
+            dict_[ans]=head
+            head=head.next
+        head=dummy
+        ans=0
+        while head:
+            ans+=head.val
+            head.next=dict_[ans].next
+            head=head.next
+        return dummy.next     
