0cc6e6657afc20258ab03ebce16c60d55a0af03f
R-Vennela <vennelaramavath@gmail.com>
2024-04-01 10:38:14 +0530

    adding Leetcode_75 solutions

---
+++ LeetCode_75/2352. Equal Row and Column Pairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def equalPairs(self, grid):
+        count = {}
+        result = 0
+        
+        for i in range(len(grid)):
+            if tuple(grid[i]) not in count:
+                count[tuple(grid[i])] = 1
+            elif tuple(grid[i]) in count:
+                count[tuple(grid[i])] += 1
+        
+        for n in range(len(grid)):
+            col = [i[n] for i in grid]
+            if col in grid:
+                result += count[tuple(col)]
+        return result
