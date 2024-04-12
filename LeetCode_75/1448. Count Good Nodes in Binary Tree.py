c78a15b07eaf89e8973ab99a7756e67be4d85c76
R-Vennela <vennelaramavath@gmail.com>
2024-04-12 17:47:08 +0530

    adding Leetcode_75 solutions

---
+++ LeetCode_75/1448. Count Good Nodes in Binary Tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def goodNodes(self, root):
+    
+        if not root:
+            return 0
+        
+        count = 0
+        q = deque([(root, root.val)])
+        
+        while q:
+            temp, maxi = q.popleft()
+            
+            if temp.val >= maxi:
+                count += 1
+            
+            if temp.left:
+                if maxi <= temp.left.val:
+                    q.append((temp.left, temp.left.val))
+                else:
+                    q.append((temp.left, maxi))
+            
+            if temp.right:
+                if maxi <= temp.right.val:
+                    q.append((temp.right, temp.right.val))
+                else:
+                    q.append((temp.right, maxi))
+        
+        return count
