09238e888148d4203a29b65e4895c573b679a2c2
R-Vennela <vennelaramavath@gmail.com>
2024-04-16 17:14:04 +0530

    adding Leetcode_75 solutions

---
+++ LeetCode_75/199. Binary Tree Right Side View.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rightSideView(self, root):
+        if root is None:
+            return []
+        
+        queue = deque([root,])
+        rightside = []
+        
+        while queue:
+            level_length = len(queue)
+
+            for i in range(level_length):
+                node = queue.popleft()
+
+                # if it's the rightmost element
+                if i == level_length - 1:
+                    rightside.append(node.val)
+                    
+                # add child nodes in the queue 
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+        return rightside
