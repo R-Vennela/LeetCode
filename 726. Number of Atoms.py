0dd283e8737ea7d1f3ab812080d627f2cd4f9689
R-Vennela <vennelaramavath@gmail.com>
2024-07-14 16:50:07 +0530

    adding Leetcode solutions

---
+++ 726. Number of Atoms.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def countOfAtoms(self, formula):
+        dt = defaultdict(int)
+        stack = [1]
+        digits = ""
+        lowers = ""
+        for element in formula[::-1]:
+            if element.isdigit():
+                digits = element + digits
+            elif element.islower():
+                lowers = element + lowers
+            elif element == ")":
+                stack.append(stack[-1]*int(digits or 1))
+                digits = ""
+            elif element == "(":
+                stack.pop()
+            else:
+                element = element + lowers
+                dt[element] = dt[element]+stack[-1]*(int(digits or 1))
+                digits = ""
+                lowers = ""
+        result = []
+        for key, value in sorted(dt.items()):
+            if value == 1:
+                value = ""
+            result.append(key)
+            result.append(str(value))
+        return "".join(result)
